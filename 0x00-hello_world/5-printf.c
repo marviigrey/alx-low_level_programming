d317e77d6e50bac68f49a2aaef78422868560959
marviigrey <marvellousfrederick@gmail.com>
2022-06-06 07:14:34 -0700

    prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * main- calls out function and prints stdout
+ * return-returns 0 value (success)
+ */
 int main(void)	{
-	printf("with proper grammar, but the outcome is a piece of art,
-			
-			"};
-			return(0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return(0);
+}
